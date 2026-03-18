f84dc1d9b784ee25f815c7a6b7ba46b21efcabf2
Kriti Aggarwal <154600653+01-kriti@users.noreply.github.com>
2026-03-18 12:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.34%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,78 @@
-class Solution {
-public:
-int lowerBound(vector<int> arr, int n, int x) {
-	int low=0,high=n-1;
-	int ans=n;
-	while(low<=high){
-		int mid=(low+high)/2;
-		if(arr[mid]>= x){
-			ans=mid;
-			high=mid-1;
-		}
-		else low=mid+1;
-	}
-	return ans;
+// class Solution {
+// public:
+// int lowerBound(vector<int> arr, int n, int x) {
+// 	int low=0,high=n-1;
+// 	int ans=n;
+// 	while(low<=high){
+// 		int mid=(low+high)/2;
+// 		if(arr[mid]>= x){
+// 			ans=mid;
+// 			high=mid-1;
+// 		}
+// 		else low=mid+1;
+// 	}
+// 	return ans;
 	
-}
-int upperBound(vector<int> &arr, int n, int x){
+// }
+// int upperBound(vector<int> &arr, int n, int x){
+// 	int low=0,high=n-1;
+// 	int ans=n;
+// 	while(low<=high){
+// 		int mid=(low+high)/2;
+// 		if(arr[mid]>x){
+// 			ans=mid;
+// 			high=mid-1;
+// 		}
+// 		else low=mid+1;
+// 	}
+// 	return ans;
+// }
+//     vector<int> searchRange(vector<int>& arr, int x) {
+//         int n=arr.size();
+//         int lb=lowerBound(arr,n,x);
+//         if(lb==n || arr[lb]!=x)return {-1,-1};
+//         return {lb,upperBound(arr,n,x)-1};
+//     }
+// };
+
+int first(vector<int>&arr,int n,int x){
+	int ans=-1;
 	int low=0,high=n-1;
-	int ans=n;
 	while(low<=high){
 		int mid=(low+high)/2;
-		if(arr[mid]>x){
+		if(arr[mid]==x){
 			ans=mid;
 			high=mid-1;
 		}
-		else low=mid+1;
+		else if(arr[mid]<x) low=mid+1;
+		else high=mid-1;
 	}
 	return ans;
 }
-    vector<int> searchRange(vector<int>& arr, int x) {
+int last(vector<int>&arr,int n,int x){
+	int ans=-1;
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]==x){
+			ans=mid;
+			low=mid+1;
+		}
+		else if(arr[mid]<x) low=mid+1;
+		else high=mid-1;
+	}
+	return ans;
+}
+
+class Solution{
+public:
+
+    vector<int>searchRange(vector<int>&arr,int k){
         int n=arr.size();
-        int lb=lowerBound(arr,n,x);
-        if(lb==n || arr[lb]!=x)return {-1,-1};
-        return {lb,upperBound(arr,n,x)-1};
+        int f=first(arr,n,k);
+	    if(f==-1)return{-1,-1};
+	    int l=last(arr,n,k);
+	    return{f,l};
     }
+
 };
